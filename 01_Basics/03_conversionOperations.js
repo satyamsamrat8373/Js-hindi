b8da08cdd5a17234d753e5054d3764768b8c135f
Satyam Samrat <146314284+satyamsamrat8373@users.noreply.github.com>
2024-12-09 05:41:09 +0000

    Conversion of datatypes

---
+++ 01_Basics/03_conversionOperations.js
@@ -0,0 +1,24 @@
+let score = true
+
+console.log(typeof score);
+console.log(typeof (score));
+
+let valueIn = Number(score)
+console.log(typeof valueIn);
+console.log(valueIn)
+
+// "33" = 33
+// "33abc = nan(not a number)
+// true = 1; false = 0;
+
+let islogged = "satyam";
+let value2 = Boolean(islogged)
+console.log(value2);
+
+// 1 = true; 0 = false;
+// "satyam" = true ; "" = false
+
+let number = 33;
+let stringnum = String(number);
+console.log(stringnum);
+console.log(typeof stringnum);
