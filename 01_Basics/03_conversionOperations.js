60b775818ac4ab2843680e3eccec220846333619
Satyam Samrat <146314284+satyamsamrat8373@users.noreply.github.com>
2024-12-10 12:48:54 +0000

    DataTypes

--- 01_Basics/03_conversionOperations.js
+++ 01_Basics/03_conversionOperations.js
@@ -1,24 +1,47 @@
 let score = true
 
-console.log(typeof score);
-console.log(typeof (score));
+// console.log(typeof score);
+// console.log(typeof (score));
 
 let valueIn = Number(score)
-console.log(typeof valueIn);
-console.log(valueIn)
+// console.log(typeof valueIn);
+// console.log(valueIn)
 
 // "33" = 33
 // "33abc = nan(not a number)
 // true = 1; false = 0;
 
 let islogged = "satyam";
-let value2 = Boolean(islogged)
-console.log(value2);
+// let value2 = Boolean(islogged)
+// console.log(value2);
 
 // 1 = true; 0 = false;
 // "satyam" = true ; "" = false
 
 let number = 33;
 let stringnum = String(number);
-console.log(stringnum);
-console.log(typeof stringnum);
+// console.log(stringnum);
+// console.log(typeof stringnum);
+
+let value = 3;
+let negvalue = -value;
+// console.log(negvalue);
+
+// console.log(2 + 2);
+// console.log(2 - 2);
+// console.log(2 * 2);
+// console.log(2 ** 3); //means 2 power 3
+// console.log(2 / 3);
+// console.log(2 % 3);
+
+let str1 = "satyam"
+let str2 = "samrat"
+// console.log(str1 + str2)
+
+console.log(1 + "2");
+console.log("1" + 2);
+console.log("1" + 2 + 2); //122
+console.log(1 + 2 + "2"); //32
+
+console.log(+true);
+//console.log(true+); //error
