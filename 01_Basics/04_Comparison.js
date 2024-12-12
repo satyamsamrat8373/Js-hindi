243d084d632ddc60296e89936cada2888b671ed3
Satyam Samrat <146314284+satyamsamrat8373@users.noreply.github.com>
2024-12-12 13:04:04 +0000

    Comparison

---
+++ 01_Basics/04_Comparison.js
@@ -0,0 +1,20 @@
+// console.log(2 > 1); //true
+// console.log(2 >= 1);  //true
+// console.log(2 < 1);  //false
+// console.log(2 == 1); //false
+// console.log(2 != 1); //true
+
+// console.log("2" > 1);  //true
+// console.log("02" > 1); //true
+
+
+// These all below conditions gives output as False.
+console.log(null == 0);
+console.log(null > 0);
+console.log(null < 0);
+
+console.log(undefined == 0);
+console.log(undefined > 0);
+console.log(undefined < 0);
+
+console.log("2" === 2);
