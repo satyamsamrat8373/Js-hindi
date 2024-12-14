011ddbe590f556b9faad6db893ac61b5c99c2619
Satyam Samrat <146314284+satyamsamrat8373@users.noreply.github.com>
2024-12-14 13:38:05 +0000

    SomeChange

--- 01_Basics/01_variable.js
+++ 01_Basics/01_variable.js
@@ -1,3 +1,46 @@
+
+    /* 1. IN JavaScript as a programmer we no need to declare data types for variables.
+
+    2. JavaScript itself will assign the data type for variables based on the value which we have stored in it. */
+
+    // var, let, const:
+    {
+        var items = 20; // Allowed
+        console.log(items);
+    }
+    var items = 400; // Reassignment Allowed
+    console.log(items); 
+
+    {
+        let product_count = 300;
+        console.log('Product Count is: ', product_count);
+        product_count = 900;
+        console.log('Product Count is: ', product_count);
+    }
+    // console.log('Product Count is: ', product_count); // not allowed
+
+    {
+        const name ='kodnest';
+        console.log('The name is ', name);
+        // name ='Technologies';   //ERROR Not-allowed
+        // console.log('The name is ', name);
+    }
+    //console.log('The name is ', name);
+// <!-- 
+//     Var:
+
+//     1. The variables which are declared outside the class using var can be accessed
+//     inside the same block and outside the block as well.
+    
+//     2. Reassignment for the variables is allowed.
+
+//     LET: 
+//     1. The variables which are declared using let can be accessed inside and can be reassigned 
+//         to the new value in the same block.
+
+//     CONST:
+//     1. The variables which are declared using const cannot be accessed outside the block
+//         and cannot be reassigned inside or outside the block.
 const accountId = 12345;
 let accountGmail = "satyam@gmail.com";
 var password = "0000";
--- 01_Basics/05_functiondec.js
+++ 01_Basics/05_functiondec.js
@@ -53,3 +53,39 @@
             console.log(a / b);
         }
         arrow(12,5);
+
+        //WAP to return addition of two number. -->
+
+    // Without Parameters
+    function add() {
+        var a = 10;
+        var b = 20;
+        console.log("Addition is : ", a + b);
+    }
+    // Calling of all Function:
+    add();
+
+    // With Parameters and Without return
+    function sub(a,b) {
+        console.log("Substraction is: ", a - b);
+    }
+    // Calling substract function
+    sub(42,23);
+
+    // Without Parameters and with return
+    function multiply() {
+        var a = 10;
+        var b = 5;
+        return a * b;
+    }
+    // Calling multiply function
+    multiply();
+    console.log(multiply());
+
+    // With Parameters and without return statement:
+    function div(a,b) {
+        return a / b;
+    }
+    // Calling fundiv()
+    div(32,12)
+    console.log(div(32,12));
